5eb1188511e389bb4ab8f79ab0d75927e1aa57a7
Ritik <ritikintelligence7231@gmail.com>
2022-05-20 18:32:22 +0530

    splash complete

---
+++ src/electricity/billing/system/Splash.java
@@ -0,0 +1,19 @@
+ 
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Splash extends JFrame{
+       
+    Splash()
+    {
+        setSize(400,400);
+        setVisible(true);
+        setLocation(500,200);
+    }
+    
+    public static void main(String[] args)
+    {
+        new Splash();
+    }
+}
