15eeb048c7c1bb188918a857fa4a251cbcca9d06
Ritik <ritikintelligence7231@gmail.com>
2022-05-20 18:43:27 +0530

    Intro Image splash complete

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -2,14 +2,50 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Splash extends JFrame{
-       
+public class Splash extends JFrame implements Runnable{
+    
+    Thread t;
     Splash()
     {
-        setSize(400,400);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg")); 
+        Image i2 = i1.getImage().getScaledInstance(730,550,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);        
+        JLabel im = new JLabel(i3);
+        
+        add(im);
+        
         setVisible(true);
-        setLocation(500,200);
+        int x=1;
+        for(int i=2;i<600;i+=4,x++)
+        {
+            setSize(i+x,i);
+            setLocation(600-((i+x)/2),350-(i/2));           
+        
+            try{
+                Thread.sleep(2);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        } 
+        t = new Thread(this);
+        t.start();
+    }
+    
+    // threading abstract method
+    public void run()
+    {
+        try{
+            Thread.sleep(7000);
+            setVisible(false);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
     
     public static void main(String[] args)
